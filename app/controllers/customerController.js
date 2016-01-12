bfc29f9eda571013a2dc4a0bdda8a3f360f3eff5
sourabhdattawad <sourabhdattawad@gmail.com>
2016-01-12 23:22:45 +0530

    initial commit

---
+++ app/controllers/customerController.js
@@ -0,0 +1,25 @@
+
+function customerController($scope){
+		
+		$scope.customers = [
+
+		{
+			joined:'12-01-2014', 
+			name:'python',
+			city:'belgaum'
+		},
+		{
+			joined:'13-01-2014',
+			name:'java',
+			city:'banglore'
+		},
+		{
+			joined:'14-01-2014', 
+			name:'ruby',
+			city:'pune'
+		}
+
+		];
+	
+
+}
